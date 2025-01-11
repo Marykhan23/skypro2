654395b9cbf4386176b9300893a67792cb1a2cae
Maria <marykhan23@gmail.com>
2025-01-11 22:26:21 +1100

    added sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -8,5 +8,9 @@ def filter_by_state(dics: List, state = 'EXECUTED') -> List:
             new_dict.append(i)
     return new_dict
 
-#dic1 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-#print(filter_by_state(dic1, 'CANCELED'))
+
+def sort_by_date(dics: List, ascending=True) -> List:
+    """Returns sorted list of dictionaries by Date"""
+    list_sorted = []
+    list_sorted = sorted(dics, reverse=ascending, key = lambda x: x.get('date'))
+    return list_sorted
